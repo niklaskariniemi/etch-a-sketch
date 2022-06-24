0dd821a37f0ecc243e3a2a1d0138ce2292010882
Niklas Kariniemi <niklaskariniemi@gmail.com>
2022-06-24 11:37:31 +0300

    Add event listener for grid size button

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-// add button when clicked prompts the user
+// initialize variables
+let gridSize = 0;
+
+// initialize grid
 
 
-// create grid
-const cdiv = document.querySelector('.container');
+// function creates grid
+function grid( gridSize ){
+    const cdiv = document.querySelector( '.container-grid' );
 
-for (let i = 1; i < gridsize; i++) {
-    const div = document.createElement('div');
-    div.style.cssText = "background:#ffebcd; height: 25px; width: 25px"; 
+    for (let i = 0; i < gridSize; i++) {
+        const div = document.createElement('div');
+        div.style.cssText = "height: 25px; width: 25px"; 
 
-    // add event listeners for when hovering over grid
-    div.addEventListener('mouseover', () => {
-        div.style.cssText = "background: #1ecbe1";
-    });
-    cdiv.appendChild(div);
+        // add event listeners for when hovering over grid
+        div.addEventListener('mouseover', () => {
+            div.style.cssText = "background: #1ecbe1";
+        });
+        cdiv.appendChild(div);
+    }
 }
+
+// button when clicked prompts the user
+const btn = document.querySelector( '.size-button' );
+btn.addEventListener('click', () => {
+    gridSize = prompt( "Enter grid size" );
+    gridSize = gridSize * gridSize;
+    grid( gridSize );
+});
